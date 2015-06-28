47c829991c88e2a7f487a712dec278a2a757208c
Roy Art <royart@me.com>
2015-06-28 17:36:25 -0400

    Client side initial check in Merging grunt file Adding client side git ignores

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,158 @@
-"use strict";
-
-var gulp = require("gulp");
+/* jshint node:true */
+'use strict';
+// generated on 2015-06-28 using generator-gulp-webapp 0.2.0
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
 var ts = require("gulp-typescript");
 var sourceMaps = require('gulp-sourcemaps');
 var del = require('del');
 var nodemon = require('gulp-nodemon');
+var sass = require('gulp-sass');
 
-gulp.task('serve', ['build-server'], function() {
-	nodemon({
-		script: 'build/server/app.js',
-		ext: 'js html',
-		env: { 'NODE_ENV': 'development' },
-		execMap: {
-			"js": "node"
-		},
-		watch: [
-			'build/server/'
-		]
-	});
+gulp.task('styles', function () {
+    return gulp.src('app/styles/main.scss')
+        .pipe($.plumber())
+        .pipe(sass({
+            style: 'expanded',
+            precision: 10
+        }))
+        .pipe($.autoprefixer({browsers: ['last 1 version']}))
+        .pipe(gulp.dest('.tmp/styles'));
 });
 
+gulp.task('jshint', function () {
+    return gulp.src('app/scripts/**/*.js')
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish'))
+        .pipe($.jshint.reporter('fail'));
+});
+
+gulp.task('html', ['styles'], function () {
+    var lazypipe = require('lazypipe');
+    var cssChannel = lazypipe()
+        .pipe($.csso)
+        .pipe($.replace, 'libs/bootstrap-sass-official/assets/fonts/bootstrap', 'fonts');
+    var assets = $.useref.assets({searchPath: '{.tmp,app}'});
+
+    return gulp.src('app/*.html')
+        .pipe(assets)
+        .pipe($.if('*.js', $.uglify()))
+        .pipe($.if('*.css', cssChannel()))
+        .pipe(assets.restore())
+        .pipe($.useref())
+        .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function () {
+    return gulp.src('app/images/**/*')
+        .pipe($.cache($.imagemin({
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('fonts', function () {
+    return gulp.src(require('main-bower-files')().concat('app/fonts/**/*'))
+        .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
+        .pipe($.flatten())
+        .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('extras', function () {
+    return gulp.src([
+        'app/*.*',
+        '!app/*.html',
+        'node_modules/apache-server-configs/dist/.htaccess'
+    ], {
+        dot: true
+    }).pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', require('del').bind(null, ['.tmp', 'dist/*', 'build/*']));
+
+/*gulp.task('connect', ['styles'], function () {
+    var serveStatic = require('serve-static');
+    var serveIndex = require('serve-index');
+    var app = require('connect')()
+        .use(require('connect-livereload')({port: 35729}))
+        .use(serveStatic('.tmp'))
+        .use(serveStatic('app'))
+        // paths to libs should be relative to the current file
+        // e.g. in app/index.html you should use ../libs
+        .use('/libs', serveStatic('libs'))
+        .use(serveIndex('app'));
+
+    require('http').createServer(app)
+        .listen(9000)
+        .on('listening', function () {
+            console.log('Started connect web server on http://localhost:9000');
+        });
+});*/
+
+gulp.task('serve', ['build-server', 'watch'], function () {
+    require('gulp-nodemon')({
+        script: 'build/server/app.js',
+        ext: 'js html',
+        env: { 'NODE_ENV': 'development' },
+        execMap: {
+            "js": "node"
+        },
+        watch: [
+            'build/server/'
+        ]
+    });
+});
+
+// inject bower components
+gulp.task('wiredep', function () {
+    var wiredep = require('wiredep').stream;
+
+    gulp.src('app/styles/*.scss')
+        .pipe(wiredep())
+        .pipe(gulp.dest('app/styles'));
+
+    gulp.src('app/*.html')
+        .pipe(wiredep({exclude: ['bootstrap-sass-official']}))
+        .pipe(gulp.dest('app'));
+});
+
+gulp.task('watch', /*['connect'],*/ function () {
+    $.livereload.listen();
+
+    // watch for changes
+    gulp.watch([
+        'app/*.html',
+        '.tmp/styles/**/*.css',
+        'app/scripts/**/*.js',
+        'app/images/**/*'
+    ]).on('change', $.livereload.changed);
+
+    gulp.watch('app/styles/**/*.scss', ['styles']);
+    gulp.watch('bower.json', ['wiredep']);
+});
+
+gulp.task('build', ['build-server', 'jshint', 'html', 'images', 'fonts', 'extras'], function () {
+    return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+});
+
+gulp.task('default', ['watch-build', 'serve']);
+
 gulp.task('build-server', function() {
-	var tsProject = ts.createProject('tsconfig.json', { typescript: require('typescript'), sourceMap: true, sortOutput: true });
-	
-	var tsResult = tsProject.src() 
-		.pipe(sourceMaps.init())
-        .pipe(ts(tsProject));
-    
-    return tsResult.js
-		.pipe(sourceMaps.write())
-		.pipe(gulp.dest('build'));
+  var tsProject = ts.createProject('tsconfig.json', { typescript: require('typescript'), sourceMap: true, sortOutput: true });
+
+  var tsResult = tsProject.src()
+    .pipe(sourceMaps.init())
+    .pipe(ts(tsProject));
+
+  return tsResult.js
+    .pipe(sourceMaps.write())
+    .pipe(gulp.dest('build'));
 });
 
 gulp.task('watch-build', ['build-server'], function() {
-	gulp.watch('server/**/*.ts', ['build-server']);
-});
-
-gulp.task('clean', function() {
-	del(['build/*'], function(err, paths) {
-		console.log('Deleted files/folders:\n', paths.join('\n'));
-	});
+  gulp.watch('server/**/*.ts', ['build-server']);
 });
 
 gulp.task('test', ['build-server']);
-
-gulp.task('build', ['build-server']);
-
-gulp.task('default', ['watch-build', 'serve']);
